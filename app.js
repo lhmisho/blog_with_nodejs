0bbcee1201129d1ee451a36329282ee11087108f
lhmisho <lokman.hossen@circlefintech.com>
2021-04-19 00:02:17 +0600

    signup page initials

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const app = express()
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
+// home roouter
+app.get('/', (req, res) => {
+    res.render('pages/auth/signup.ejs', {'title': 'Create a new account'})
+})
+
 // middleware array
 const middleware = [
     morgan('dev'),
@@ -21,6 +26,6 @@ app.use(middleware)
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
-    console.log('Server is running at')
+    console.log(`Server is running at ${PORT}`)
 })
     
