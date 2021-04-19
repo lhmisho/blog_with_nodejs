760ec78bb7059afeed0c97f1b423ef3687b6f638
lhmisho <lokman.hossen@circlefintech.com>
2021-04-19 15:30:34 +0600

    login controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,7 +10,7 @@ exports.signupPostController = async (req, res, next) => {
     let { username, email, password, confirmPassword} = req.body
     
     try{
-        
+
         hashedPassword = await bcrypt.hash(password, 11)
         let user = new User({
             username, email, password: hashedPassword
@@ -26,10 +26,30 @@ exports.signupPostController = async (req, res, next) => {
 }
 
 exports.loginGetController = (req, res, next) => {
-
+    res.render('pages/auth/login.ejs', {title: 'Login to your account'})
 }
 
-exports.loginPostController = (req, res, next) => {
+exports.loginPostController = async (req, res, next) => {
+    let { email, password } = req.body
+
+    try{
+        let user = await User.findOne({email: email})
+        if(!user){
+            res.json({message: "Invalid Credentials"})
+        }
+
+        let match = bcrypt.compare(password, user.password)
+        if(!match){
+            res.json({message: "Invalid Credentials"})
+        }
+        console.log('Logged in user', user)
+        res.render('pages/auth/login.ejs', {title: 'Login to your account'})
+
+    }catch(e){
+        console.log(e)
+        next(e)
+    }
+
 
 }
 
