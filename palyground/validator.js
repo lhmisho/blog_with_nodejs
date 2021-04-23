7b6d38bc775321396b60b7da6fdab04279ecffc7
lhmisho <lokman.hossen@circlefintech.com>
2021-04-24 00:02:55 +0600

    add sanitizer

--- palyground/validator.js
+++ palyground/validator.js
@@ -6,39 +6,44 @@ router.get('/validator', (req, res, next) => {
     res.render('playground/signup.ejs', { title: 'Signup Playground' })
 })
 
-router.post('/validator', 
+router.post('/validator',
     [
         check('username')
             .not()
             .isEmpty()
             .withMessage('Username cannot be empty')
             .isLength({ max: 15 })
-            .withMessage('Username cannot be greater than 15 character'),
+            .withMessage('Username cannot be greater than 15 character')
+            .trim(),
         check('email')
             .isEmail()
-            .withMessage('Please provide a valid email'),
+            .withMessage('Please provide a valid email')
+            .normalizeEmail(),
         check('password').custom(value => {
-            if(value.length < 5){
+            if (value.length < 5) {
                 throw new Error('Paasword length is must be greater than 5 character')
             }
             return true
         }),
-        check('confirmPassword').custom((value, {req}) => {
-            if(value !== req.body.password){
+        check('confirmPassword').custom((value, { req }) => {
+            if (value !== req.body.password) {
                 throw new Error('Password does not matched')
             }
             return true
         })
     ],
     (req, res, next) => {
-    let errors = validationResult(req)
+        let errors = validationResult(req)
 
-    const formatter = (error) => error.msg
+        const formatter = (error) => error.msg
 
-    console.log(errors)
-    console.log(errors.formatWith(formatter).mapped())
-    res.render('playground/signup.ejs', { title: 'Signup Playground' })
-})
+        console.log(errors)
+        console.log(errors.formatWith(formatter).mapped())
+
+        console.log(req.body)
+
+        res.render('playground/signup.ejs', { title: 'Signup Playground' })
+    })
 
 
 module.exports = router
